5e078a96171131703a09f5bc99f1da562c73be55
c-buquet <charles@agence-osez.com>
2023-11-19 23:46:21 +0100

    Update scroll-content-x.js

--- web/app/themes/remarquable/resources/scripts/features/scroll-content-x.js
+++ web/app/themes/remarquable/resources/scripts/features/scroll-content-x.js
@@ -1,22 +1,33 @@
 $(".scroll-content-slides").on("mousedown", function (event) {
   event.preventDefault();
 
-  var isDragging = true;
   var startX = event.pageX - $(this).offset().left;
   var scrollLeft = $(this).scrollLeft();
+  var momentum = 0.8; // Facteur d'inertie
 
   $(this).css("cursor", "grabbing");
 
-  $(document).on("mousemove", function (event) {
-    if (isDragging) {
-      var x = event.pageX - $(".scroll-content-slides").offset().left;
-      var walk = (x - startX) * 2; // Ajustez la vitesse du défilement
-      $(".scroll-content-slides").scrollLeft(scrollLeft - walk);
-    }
+  $(document).on("mousemove", function handleMouseMove(event) {
+    var x = event.pageX - $(".scroll-content-slides").offset().left;
+    var walk = (x - startX) * 2; // Ajustez la vitesse du défilement
+    $(".scroll-content-slides").scrollLeft(scrollLeft - walk);
   });
 
   $(document).on("mouseup", function () {
-    isDragging = false;
     $(".scroll-content-slides").css("cursor", "grab");
+    $(document).off("mousemove");
+
+    // Effet de défilement inertiel après le relâchement
+    function inertialScroll() {
+      if (Math.abs(momentum) > 0.1) {
+        $(".scroll-content-slides").scrollLeft(
+          $(".scroll-content-slides").scrollLeft() - momentum
+        );
+        momentum *= 0.95; // Ajustez le facteur de décélération
+        requestAnimationFrame(inertialScroll);
+      }
+    }
+
+    inertialScroll();
   });
 });
