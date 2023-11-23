9fa2c74504bbc17c1401936e56cc01fee7eec656
c-buquet <charles@agence-osez.com>
2023-11-23 14:43:18 +0100

    maj css

--- web/app/themes/remarquable/resources/views/blocks/our-experts.blade.php
+++ web/app/themes/remarquable/resources/views/blocks/our-experts.blade.php
@@ -7,7 +7,7 @@
 
       <div class="flex flex-col md:flex-row gap-6">
         @foreach ($fields['cards'] as $card)
-            <div class="w-full md:w-1/2 relative flex flex-col items-center justify-center group min-h-[370px] max-h-[470px]">
+            <div class="w-full md:w-1/2 relative flex flex-col items-center justify-center group lg:min-h-[370px] max-h-[470px]">
               <img class="absolute top-0 left-0 object-cover h-full w-full" src="{{ $card['image']['url'] }}" alt="{!! $card['image']['title'] !!}">
               <div class="absolute top-0 left-0 w-full h-full bg-cards-filter"></div>
 
@@ -16,7 +16,7 @@
                 <x-icons.horizontal-bar color="bg-white-cloud" />
               </div>
 
-              <div class="h-full opacity-0 group-hover:opacity-100 duration-500 z-20 bg-green-primary py-20 px-16 flex flex-col gap-4 items-center justify-center border">
+              <div class="h-full opacity-0 group-hover:opacity-100 duration-500 z-20 bg-green-primary py-6 lg:py-20 px-6 lg:px-16 flex flex-col gap-4 items-center justify-center border">
                 <div class="font-playfair font-bold text-2xl tracking-wide uppercase text-center">{!! $card['title'] !!}</div>
                 <x-icons.horizontal-bar color="bg-white-cloud" />
                 <div class="text-xl text-center">{!! $card['text'] !!}</div>
