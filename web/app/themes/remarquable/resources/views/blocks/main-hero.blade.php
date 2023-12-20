7fef342e53eec306836a544c2c925471c74bc08f
c-buquet <charles@agence-osez.com>
2023-12-20 19:34:18 +0100

    Update main-hero.blade.php

--- web/app/themes/remarquable/resources/views/blocks/main-hero.blade.php
+++ web/app/themes/remarquable/resources/views/blocks/main-hero.blade.php
@@ -6,7 +6,7 @@
 <section class="{{ $classes }} {{ is_admin() ? 'h-[700px]' : 'h-[700px] md:h-screen' }} relative" data-gsap>
   <div class="lg:ml-[80px] h-full">
     <div class="container-right h-full">
-      <div class="relative z-10 flex items-end md:items-center h-full pb-20 md:pb-0 pt-24">
+      <div class="relative z-10 flex items-end md:items-center h-full md:pb-0 pt-24">
         <div class="w-10/12 lg:w-[590px] pt-28 lenis-title" data-anim data-from='{"autoAlpha":0, "duration": 1}' data-to='{"autoAlpha":1,"duration": 1.25}'>{!! $title !!}</div>
 
         <div class="w-full hidden md:flex justify-center absolute bottom-10 left-1/2 -translate-x-2/4">
@@ -17,6 +17,6 @@
   </div>
 
   <img class="hidden sm:block w-full h-full object-cover object-right-bottom md:object-right absolute top-0 left-0" src="{{ $hero_image['url'] }}" alt="{!! $hero_image['title'] !!}">
-  <img class="sm:hidden w-full h-full object-cover object-bottom md:object-right absolute top-0 left-0" src="{{ $hero_image_mobile['url'] }}" alt="{!! $hero_image_mobile['title'] !!}">
+  <img class="sm:hidden w-full h-full object-cover object-right-bottom md:object-right absolute top-0 left-0" src="{{ $hero_image_mobile['url'] }}" alt="{!! $hero_image_mobile['title'] !!}">
   <div class="bg-main-hero-filter w-full h-full absolute top-0 left-0"></div>
 </section>
