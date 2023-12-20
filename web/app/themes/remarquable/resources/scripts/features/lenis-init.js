bb5fdd4202d621e13101b0cd066db041e372d5dd
c-buquet <charles@agence-osez.com>
2023-12-20 19:59:30 +0100

    maj fix bug scroll to top

---
+++ web/app/themes/remarquable/resources/scripts/features/lenis-init.js
@@ -0,0 +1,55 @@
+import Lenis from "@studio-freight/lenis";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+//active le js seulement en front-end !
+if (window.location.href.indexOf("/wp") === -1) {
+  const lenis = new Lenis({});
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+  requestAnimationFrame(raf);
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".main-hero",
+      start: "top top",
+      end: "bottom center",
+      scrub: true,
+    },
+  });
+
+  tl.fromTo(".main-hero .lenis-title", { y: "-25%" }, { y: "-45%" }, 0).fromTo(
+    ".main-hero .lenis-title",
+    { y: "-25%" },
+    { y: "-45%" },
+    0
+  );
+
+  //MAIN POPUP PARAMS
+  $(".open-popup-entire-screen").click(function () {
+    if (window.innerWidth > 1024) {
+      lenis.stop();
+    }
+  });
+
+  $(".close-popup-entire-screen").click(function () {
+    if (window.innerWidth > 1024) {
+      lenis.start();
+    }
+  });
+
+  $(".scroll-content").on("wheel", function (event) {
+    // Récupérer l'élément .scroll-content
+    var scrollContent = $(this);
+
+    // Récupérer la valeur actuelle de la position de défilement
+    var currentScrollTop = scrollContent.scrollTop();
+
+    // Définir la nouvelle position de défilement en fonction de la direction de la molette
+    scrollContent.scrollTop(currentScrollTop + event.originalEvent.deltaY);
+  });
+}
