82c0b5e1518924fec151f57357461dfef4d030ff
c-buquet <charles@agence-osez.com>
2024-06-16 23:31:16 +0200

    Update shortcode.php

--- web/app/plugins/metal-prices/includes/shortcode.php
+++ web/app/plugins/metal-prices/includes/shortcode.php
@@ -70,154 +70,140 @@ function metal_prices_shortcode($atts) {
     </div>
 
     <script>
-        jQuery(document).ready(function($) {
-            function fetchAndRenderChart(metal, period) {
-                $.ajax({
-                    url: `http://127.0.0.1:8000/api/metal/${metal}/history?interval=${period}`,
-                    method: 'GET',
-                    success: function(data) {
-                        var metalName = getMetalName(metal);
-                        var metalColor = getMetalColor(metal);
+    jQuery(document).ready(function($) {
+        function fetchAndRenderChart(metal, period) {
+            $.ajax({
+                url: `http://127.0.0.1:8000/api/metal/${metal}/history?interval=${period}`,
+                method: 'GET',
+                success: function(data) {
+                    var metalName = getMetalName(metal);
+                    var metalColor = getMetalColor(metal);
 
-                        // Calculate the variation
-                        var currentPrice = data[data.length - 1].price;
-                        var previousPrice = data[data.length - 2].price;
-                        var priceChange = currentPrice - previousPrice;
-                        var percentageChange = (priceChange / previousPrice) * 100;
-                        var changeClass = priceChange >= 0 ? 'positive-change' : 'negative-change';
-                        var percentageChangeFormatted = (priceChange >= 0 ? '+' : '') + percentageChange.toFixed(2);
+                    $('#current-price-title').text(`Prix courant ${metalName}: ${data[data.length - 1].price} €`);
 
-                        $('#current-price-title').text(`Prix courant de ${metalName}: ${currentPrice} €`);
-                        $('#price-change').text(`(${priceChange >= 0 ? '+' : ''}${priceChange.toFixed(2)} €, ${percentageChangeFormatted}%)`)
-                                          .removeClass('positive-change negative-change')
-                                          .addClass(changeClass);
+                    var ctx = document.getElementById('metalPricesChart').getContext('2d');
+                    var labels = data.map(function(entry) { return entry.timestamp; });
+                    var prices = data.map(function(entry) { return entry.price; });
 
-                        // Filter data to exclude the current date and include only the last 12 months
-                        if (period === '1y') {
-                            const now = new Date();
-                            data = data.filter(entry => {
-                                const entryDate = new Date(entry.date);
-                                return entryDate < now && entryDate >= new Date(now.getFullYear() - 1, now.getMonth(), now.getDate());
-                            });
+                    if (window.metalChart) {
+                        window.metalChart.destroy();
+                    }
+
+                    var xAxisOptions = {
+                        type: 'time',
+                        time: {
+                            unit: 'day',
+                            tooltipFormat: 'dd/MM/yyyy',
+                            displayFormats: {
+                                day: 'd MMM yyyy',
+                            }
+                        },
+                        ticks: {
+                            color: '#ffffff' // Color for x-axis labels
+                        },
+                        grid: {
+                            color: 'rgba(255, 255, 255, 0.1)' // Light gray grid lines
                         }
+                    };
 
-                        var ctx = document.getElementById('metalPricesChart').getContext('2d');
-                        var labels = data.map(function(entry) { return entry.date; });
-                        var prices = data.map(function(entry) { return entry.price; });
+                    if (period === '1y') {
+                        xAxisOptions.time.unit = 'month';
+                        xAxisOptions.time.displayFormats = {
+                            month: 'MMM'
+                        };
+                        xAxisOptions.time.stepSize = 1;
+                    } else if (period === '1d') {
+                        xAxisOptions.time.unit = 'hour';
+                        xAxisOptions.time.displayFormats = {
+                            hour: 'HH:mm'
+                        };
+                    }
 
-                        if (window.metalChart) {
-                            window.metalChart.destroy();
-                        }
-
-                        var xAxisOptions = {
-                            type: 'time',
-                            time: {
-                                unit: 'day',
-                                tooltipFormat: 'dd/MM/yyyy',
-                                displayFormats: {
-                                    day: 'd MMM yyyy',
+                    window.metalChart = new Chart(ctx, {
+                        type: 'line',
+                        data: {
+                            labels: labels,
+                            datasets: [{
+                                label: `Prix ${metalName} (€) `,
+                                data: prices,
+                                borderColor: metalColor,
+                                borderWidth: 2,
+                                borderDash: [],
+                                pointBackgroundColor: metalColor,
+                                pointBorderColor: metalColor,
+                                pointRadius: 3,
+                                pointHoverRadius: 7,
+                                backgroundColor: metalColor,
+                                fill: false
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                x: xAxisOptions,
+                                y: {
+                                    ticks: {
+                                        color: '#ffffff' // Color for y-axis labels
+                                    },
+                                    grid: {
+                                        color: 'rgba(255, 255, 255, 0.1)' // Light gray grid lines
+                                    }
                                 }
                             },
-                            ticks: {
-                                color: '#ffffff' // Color for x-axis labels
+                            plugins: {
+                                legend: {
+                                    labels: {
+                                        color: '#ffffff' // Color for legend labels
+                                    }
+                                }
                             },
-                            grid: {
-                                color: 'rgba(255, 255, 255, 0.1)' // Light gray grid lines
-                            }
-                        };
-
-                        if (period === '1y') {
-                            xAxisOptions.time.unit = 'month';
-                            xAxisOptions.time.displayFormats = {
-                                month: 'MMM'
-                            };
-                            xAxisOptions.time.stepSize = 1;
                         }
-
-                        window.metalChart = new Chart(ctx, {
-                            type: 'line',
-                            data: {
-                                labels: labels,
-                                datasets: [{
-                                    label: `Prix ${metalName} (€) `,
-                                    data: prices,
-                                    borderColor: metalColor,
-                                    borderWidth: 2,
-                                    borderDash: [],
-                                    pointBackgroundColor: metalColor,
-                                    pointBorderColor: metalColor,
-                                    pointRadius: 3,
-                                    pointHoverRadius: 7,
-                                    backgroundColor: metalColor,
-                                    fill: false
-                                }]
-                            },
-                            options: {
-                                scales: {
-                                    x: xAxisOptions,
-                                    y: {
-                                        ticks: {
-                                            color: '#ffffff' // Color for y-axis labels
-                                        },
-                                        grid: {
-                                            color: 'rgba(255, 255, 255, 0.1)' // Light gray grid lines
-                                        }
-                                    }
-                                },
-                                plugins: {
-                                    legend: {
-                                        labels: {
-                                            color: '#ffffff' // Color for legend labels
-                                        }
-                                    }
-                                },
-                            }
-                        });
-                    },
-                    error: function(jqXHR, textStatus, errorThrown) {
-                        console.error('Error fetching data: ', textStatus, errorThrown);
-                    }
-                });
-            }
-
-            function getMetalName(code) {
-                var metals = {
-                    'XAU': 'Or',
-                    'XAG': 'Argent',
-                    'XPT': 'Platine',
-                    'XPD': 'Palladium'
-                };
-
-                return metals[code] || code;
-            }
-
-            function getMetalColor(code) {
-                var colors = {
-                    'XAU': 'rgb(196, 171, 95)',
-                    'XAG': 'silver',
-                    'XPT': 'gray',
-                    'XPD': 'darkgray'
-                };
-
-                return colors[code] || 'black';
-            }
-
-            $('#metal-selector').on('change', function() {
-                var metal = $(this).val();
-                var period = $('#period-selector').val();
-                fetchAndRenderChart(metal, period);
+                    });
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    console.error('Error fetching data: ', textStatus, errorThrown);
+                }
             });
+        }
 
-            $('#period-selector').on('change', function() {
-                var period = $(this).val();
-                var metal = $('#metal-selector').val();
-                fetchAndRenderChart(metal, period);
-            });
+        function getMetalName(code) {
+            var metals = {
+                'XAU': 'Or',
+                'XAG': 'Argent',
+                'XPT': 'Platine',
+                'XPD': 'Palladium'
+            };
 
-            // Initial load
-            fetchAndRenderChart('<?php echo esc_js($metal); ?>', '<?php echo esc_js($period); ?>');
+            return metals[code] || code;
+        }
+
+        function getMetalColor(code) {
+            var colors = {
+                'XAU': 'rgb(196, 171, 95)',
+                'XAG': 'silver',
+                'XPT': 'gray',
+                'XPD': 'darkgray'
+            };
+
+            return colors[code] || 'black';
+        }
+
+        $('#metal-selector').on('change', function() {
+            var metal = $(this).val();
+            var period = $('#period-selector').val();
+            fetchAndRenderChart(metal, period);
         });
-    </script>
+
+        $('#period-selector').on('change', function() {
+            var period = $(this).val();
+            var metal = $('#metal-selector').val();
+            fetchAndRenderChart(metal, period);
+        });
+
+        // Initial load
+        fetchAndRenderChart('<?php echo esc_js($metal); ?>', '<?php echo esc_js($period); ?>');
+    });
+</script>
+
 
     <?php
     return ob_get_clean();
